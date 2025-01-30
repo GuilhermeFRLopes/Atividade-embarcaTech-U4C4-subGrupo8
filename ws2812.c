39206c39ae2b760eafb71d5fcd1976358d79b4da
C10UD42 <claudemir.s2940@ufob.edu.br>
2025-01-30 02:43:14 -0300

    fix - organização

--- ws2812.c
+++ ws2812.c
@@ -72,11 +72,6 @@ void set_one_led(uint8_t r, uint8_t g, uint8_t b, int numExibir)
     }
 }
 
-void ligarMatrizLeds(){
-    //implementar a logica de exibição do numero na matriz, utilize o contador "contador_numero_a_exibir"
-    set_one_led(led_r, led_g, led_b, contador_numero_a_exibir);
-}
-
 int main()
 {   
     stdio_init_all();
@@ -109,6 +104,11 @@ int main()
     }
 
     return 0;
+}
+
+void ligarMatrizLeds(){
+    //implementar a logica de exibição do numero na matriz, utilize o contador "contador_numero_a_exibir"
+    set_one_led(led_r, led_g, led_b, contador_numero_a_exibir);
 }
 
 void blinkarLedVermelho(){
